a7fe3b2ed76f4ea458d39ccbbca0e196de3a0594
Maxi-arch <mxmu025@gmail.com>
2024-11-06 12:13:32 +0300

    1stcommit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,33 @@
 // create your App component here
+import { useEffect, useState } from "react";
+
+
+
+function App() {
+    const [pics, setPics] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => {
+                setPics([data.message]); 
+            });
+    }, []); 
+
+    return (
+        <div>
+            {pics.length === 0 ? (
+                <p>Loading...</p> 
+            ) : (
+                pics.map((pic, index) => (
+                    <img key={index} src={pic} alt="A Random Dog" />
+                ))
+            )}
+        </div>
+    );
+}
+
+
+
+
+export default App;
